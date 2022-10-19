369eba111dd0dbb5b660b0ad086d8b3d2b1d5b74
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-10-19 11:33:36 +0200

    feat(destination): able to show destination on a map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,17 +2,42 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
+import MapView, { Marker } from 'react-native-maps'
 
 const MapScreen = () => {
 
+    // const [destination, setDestination] = useState({location: {lat: 50.847, lng: 4.357}})
     const [destination, setDestination] = useState(null)
 
-    useEffect(() => {
-        console.log(destination)
-    }, [])
-
   return (
-    <SafeAreaView>
+    <View>
+
+        {destination &&
+            <MapView 
+                style={styles.map}
+                mapType="mutedStandard"
+                initialRegion={{
+                    latitude: destination?.location.lat,
+                    longitude: destination?.location.lng,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }}
+            >
+                {destination?.location &&
+                    <Marker
+                        coordinate={{
+                            latitude: destination?.location.lat,
+                            longitude: destination?.location.lng,
+                        }}
+                        title="Destination"
+                        description={destination.description}
+                        idenifier="origin"
+                    />
+                }    
+            </MapView>
+        }
+        
+
         <View style={styles.searchContainer}>
            <GooglePlacesAutocomplete
             style={{textInput: styles.input}}
@@ -37,8 +62,8 @@ const MapScreen = () => {
             }}
         />  
         </View>
-       
-    </SafeAreaView>
+            
+    </View>
   )
 }
 
@@ -55,10 +80,16 @@ const styles = StyleSheet.create({
         padding: 8,
         marginLeft: '5%',
         borderRadius: 8,
+        marginTop: 40,
+        zIndex: 10
       },
     input: {
       borderColor: "#888",
       borderWidth:1
+    },
+    map: {
+        width: '100%',
+        height: '100%'
     }
   });
 
