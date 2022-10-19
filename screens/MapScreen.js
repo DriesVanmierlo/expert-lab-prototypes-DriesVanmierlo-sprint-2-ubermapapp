3502bfc017b1c9ad355242b5a1e1d1240a86bcde
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-10-19 13:52:51 +0200

    feat(directions): able to see direction line between origin and destination

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,66 +1,117 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import MapView, { Marker } from 'react-native-maps'
+import MapViewDirections from 'react-native-maps-directions'
 
 const MapScreen = () => {
 
-    // const [destination, setDestination] = useState({location: {lat: 50.847, lng: 4.357}})
+    const [origin, setOrigin] = useState(null)
     const [destination, setDestination] = useState(null)
+    const mapRef = useRef(null)
+
+    useEffect(() => {
+        if(!origin || !destination) return;
+
+        mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+            edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+        });
+    }, [origin, destination])
 
   return (
     <View>
-
-        {destination &&
+        {origin &&
             <MapView 
+                ref={mapRef}
                 style={styles.map}
                 mapType="mutedStandard"
                 initialRegion={{
-                    latitude: destination?.location.lat,
-                    longitude: destination?.location.lng,
-                    latitudeDelta: 0.05,
-                    longitudeDelta: 0.05
+                    latitude: origin.location.lat,
+                    longitude: origin.location.lng,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005
                 }}
             >
+
+                {origin && destination &&
+                    <MapViewDirections
+                        origin={origin.description}
+                        destination={destination.description}
+                        apikey={GOOGLE_MAPS_APIKEY}
+                        strokeWidth={3}
+                        strokeColor='blue'
+                        mode='WALKING'
+                    />
+                }<Marker
+                        coordinate={{
+                            latitude: origin.location.lat,
+                            longitude: origin.location.lng,
+                        }}
+                        title="Origin"
+                        description={origin.description}
+                        idenifier="origin"
+                    />
+                
+
                 {destination?.location &&
                     <Marker
                         coordinate={{
-                            latitude: destination?.location.lat,
-                            longitude: destination?.location.lng,
+                            latitude: destination.location.lat,
+                            longitude: destination.location.lng,
                         }}
                         title="Destination"
                         description={destination.description}
-                        idenifier="origin"
+                        idenifier="destination"
                     />
-                }    
+                }  
             </MapView>
         }
         
 
         <View style={styles.searchContainer}>
            <GooglePlacesAutocomplete
-            style={{textInput: styles.input}}
-            enablePoweredByContainer={false}
-            minLength={2}
-            query={{
-                key: GOOGLE_MAPS_APIKEY,
-                language: 'en'
-            }}
-            fetchDetails={true}
-            returnKeyType={"search"}
-            placeholder='Where to?'
-            nearbyPlacesAPI='GooglePlacesSearch'
-            debounce={400}
+                style={{textInput: styles.input}}
+                enablePoweredByContainer={false}
+                minLength={2}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en'
+                }}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                placeholder='Where from?'
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
 
-            onPress={(data, details = null) => {
-                setDestination({
-                    location: details.geometry.location,
-                    description: data.description
-                })
-                console.log(destination)
-            }}
-        />  
+                onPress={(data, details = null) => {
+                    setOrigin({
+                        location: details.geometry.location,
+                        description: data.description
+                    })
+                }}
+            />  
+            <GooglePlacesAutocomplete
+                style={{textInput: styles.input}}
+                enablePoweredByContainer={false}
+                minLength={2}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en'
+                }}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                placeholder='Where to?'
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+
+                onPress={(data, details = null) => {
+                    setDestination({
+                        location: details.geometry.location,
+                        description: data.description
+                    })
+                }}
+            />
         </View>
             
     </View>
